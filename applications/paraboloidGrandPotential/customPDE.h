58472e21d9317c893a454e14edc7d195c675b11e
Xander <102053371+fractalsbyx@users.noreply.github.com>
2025-02-20 12:47:36 -0500

    Print initial omega values for each phase

--- applications/paraboloidGrandPotential/customPDE.h
+++ applications/paraboloidGrandPotential/customPDE.h
@@ -21,6 +21,7 @@ public:
     ifs.close();
     isoSys.from_json(model_parameters);
     isoSys.print_parameters();
+    print_initial_energies();
   }
 
   // Function to set the initial conditions (in ICs_and_BCs.h)
@@ -97,6 +98,38 @@ private:
   interface(double x) const
   {
     return 0.5 * (1.0 + std::tanh(2.0 * x / isoSys.l_int));
+  }
+
+  void
+  print_initial_energies()
+  {
+    std::cout << "Initial omega free energies:\n";
+    for (const auto &[phase_name, phase] : isoSys.phases)
+      {
+        SystemContainer<dim, degree> sys_for_print(isoSys, userInputs);
+        sys_for_print.op_data.push_back({
+          phase_name,
+          {constV(1.), {}}
+        });
+
+        for (const auto &[comp_name, comp_info] : isoSys.phases.at(phase_name).comps)
+          {
+            double mu0 = isoSys.Vm * comp_info.k_well * (comp_info.x0 - comp_info.c_min);
+            sys_for_print.comp_data[comp_name].mu.val = constV(mu0);
+          }
+
+        sys_for_print.calculate_locals();
+
+        std::cout << phase_name << ":\n"
+                  << "Omega:\t" << sys_for_print.phase_data[phase_name].omega.val[0]
+                  << "\n";
+        for (const auto &[comp_name, comp_info] : phase.comps)
+          {
+            std::cout << "mu_" << comp_name << ":\t"
+                      << sys_for_print.comp_data[comp_name].mu.val[0] << "\n";
+          }
+        std::cout << "\n";
+      }
   }
 
   // ================================================================
