0c49f9659df9b4cd50e3e1d39aeb0a929aa05706
Xander <102053371+fractalsbyx@users.noreply.github.com>
2025-04-08 22:37:01 -0400

    made 3d, added ic template

--- applications/paraboloidGrandPotential/ICs_and_BCs.cc
+++ applications/paraboloidGrandPotential/ICs_and_BCs.cc
@@ -2,6 +2,9 @@
 // FUNCTION FOR INITIAL CONDITIONS
 // ===========================================================================
 
+#include <array>
+#include <numbers>
+
 template <int dim, int degree>
 void
 customPDE<dim, degree>::setInitialCondition([[maybe_unused]] const Point<dim>  &p,
@@ -40,18 +43,58 @@ customPDE<dim, degree>::setInitialCondition([[maybe_unused]] const Point<dim>  &
   // Make relevant geometries
   [[maybe_unused]] double circular     = interface(0.5 * (r0 * r0 - r2) / r0);
   [[maybe_unused]] double flat         = interface(0.5 * (r0 * r0 - y * y) / r0);
-  [[maybe_unused]] double bottom_strip = interface(r0 - p[1]);
+  [[maybe_unused]] double bottom_strip = interface(r0 - p[2]);
+
+  constexpr std::array<double, 3> a = {
+    {0.0, 1.0, 2.0}
+  };
+  constexpr double                     tau    = std::acos(-1.0) * 2.0;
+  double                               s      = 2.0 / (3.0 * std::sqrt(3.0));
+  double                               offset = 1.0;
+  double                               m      = 0.0;
+  std::array<std::array<double, 2>, 3> b      = {
+    {{{std::cos(tau * a[0] / 3.0), std::sin(tau * a[0] / 3.0)}},
+     {{std::cos(tau * a[1] / 3.0), std::sin(tau * a[1] / 3.0)}},
+     {{std::cos(tau * a[2] / 3.0), std::sin(tau * a[2] / 3.0)}}}
+  };
+  std::array<std::array<double, 2>, 3> d = {
+    {
+     {{std::cos(tau * a[0] / 3.0 + tau / 12.0),
+        std::sin(tau * a[0] / 3.0 + tau / 12.0)}},
+     {{std::cos(tau * a[1] / 3.0 + tau / 12.0),
+        std::sin(tau * a[1] / 3.0 + tau / 12.0)}},
+     {{std::cos(tau * a[2] / 3.0 + tau / 12.0),
+        std::sin(tau * a[2] / 3.0 + tau / 12.0)}} //
+    }
+  };
+
+  std::array<double, 3> w = {
+    {0.0, 0.0, 0.0}
+  };
+  for (uint i = 0; i < 3; i++)
+    {
+      for (uint j = 0; j < 3; j++)
+        {
+          w[i] += std::cos(tau * ((x / s0 + offset * s * d[i][0]) * b[j][0] +
+                                  (y / s0 + offset * s * d[i][1]) * b[j][1] + m));
+        }
+      w[i] *= s0 / tau;
+    }
 
   // TODO: Populate eta0 with the initial condition for each order parameters
   std::vector<double> eta0(isoSys.order_params.size(), 0.0);
   eta0[0] = 1.0 - bottom_strip;
-  eta0[1] = bottom_strip * interface(eutectic_contour_2D(x, 3, 0, s0));
-  eta0[2] = bottom_strip * interface(eutectic_contour_2D(x, 3, 1, s0));
-  eta0[3] = bottom_strip * interface(eutectic_contour_2D(x, 3, 2, s0));
+  // eta0[1] = bottom_strip * interface(eutectic_contour_2D(x, 3, 0, s0));
+  // eta0[2] = bottom_strip * interface(eutectic_contour_2D(x, 3, 1, s0));
+  // eta0[3] = bottom_strip * interface(eutectic_contour_2D(x, 3, 2, s0));
 
-  eta0[1] = bottom_strip * ((1.0 / 3.0) + 0.1 * dist(rng));
-  eta0[2] = bottom_strip * ((1.0 / 3.0) + 0.1 * dist(rng));
-  eta0[3] = bottom_strip * ((1.0 / 3.0) + 0.1 * dist(rng));
+  // eta0[1] = bottom_strip * ((1.0 / 3.0) + 0.1 * dist(rng));
+  // eta0[2] = bottom_strip * ((1.0 / 3.0) + 0.1 * dist(rng));
+  // eta0[3] = bottom_strip * ((1.0 / 3.0) + 0.1 * dist(rng));
+
+  eta0[1] = bottom_strip * interface(w[0]);
+  eta0[2] = bottom_strip * interface(w[1]);
+  eta0[3] = bottom_strip * interface(w[2]);
 
   // ---------------------------------------------------------------------
   //
